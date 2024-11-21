f48ade792291c5cd8e1704a90b787724d0dfc25f
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-11-21 13:44:28 +0530

    router, error and body imported

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
 import { useState } from 'react';
 import '../styles/App.css';
 import Header from './Header';
+import Body from './Body.jsx'
+import Error from './Error.jsx';
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <Header/>
+    <br></br>
+    <Outlet/>
+      
+    
     </>
   )
 }
+export const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+      {
+        path:"/",
+        element:<Body/>
+      }
+    ],
+    errorElement:<Error/>
+  }
+])
 
 export default App
