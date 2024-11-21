af333c0514cfb25e85b43878239fcc6f39341cc1
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-11-22 01:05:09 +0530

    apis and links added

--- src/assets/api.js
+++ src/assets/api.js
@@ -1,3 +1,5 @@
 export const API_KEY = "3a715e2372ef43199f90feaa865c8d26";
 
 export const API_US_NEWS = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=3a715e2372ef43199f90feaa865c8d26";
+
+export const SPORTS_API = "https://newsapi.org/v2/everything?q=sports&apiKey=3a715e2372ef43199f90feaa865c8d26";
---
+++ src/assets/links.js
@@ -0,0 +1,3 @@
+export const BREAKINGNEWS_URL = "https://i.pinimg.com/originals/19/3b/d7/193bd7fa3d3fba0bbf49fb2bc492249c.jpg";
+
+export const NEWSLOGO_URL ='https://logoeps.com/wp-content/uploads/2014/05/21601-news-logo-icon-vector-icon-vector-eps.png';
