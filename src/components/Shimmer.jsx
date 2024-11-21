c939f5daaef4865922f13a6cf0234f273140eb04
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-11-21 13:45:38 +0530

    shimmer card created

---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,23 @@
+import '../styles/Shimmer.css'
+const Shimmer = () => {
+    return(
+        <>
+        <div id="shimmer-card-container">
+            <div className='shimmer-cards'></div>
+            <div className='shimmer-cards'></div>
+            <div className='shimmer-cards'></div>
+            <div className='shimmer-cards'></div>
+            <div className='shimmer-cards'></div>
+            <div className='shimmer-cards'></div>
+            <div className='shimmer-cards'></div>
+            <div className='shimmer-cards'></div>
+            <div className='shimmer-cards'></div>
+            <div className='shimmer-cards'></div>
+
+            
+        </div>
+        
+        </>
+    )
+}
+export default Shimmer;
