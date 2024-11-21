cb708d22bb4293ce54fb2b14a102a43a76823cf8
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-11-21 13:27:46 +0530

    news api saved

--- src/assets/api.js
+++ src/assets/api.js
@@ -1,1 +1,3 @@
-const API_KEY = "3a715e2372ef43199f90feaa865c8d26";
+export const API_KEY = "3a715e2372ef43199f90feaa865c8d26";
+
+export const API_US_NEWS = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=3a715e2372ef43199f90feaa865c8d26";
