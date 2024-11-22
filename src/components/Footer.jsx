72abb6c2696ea310615a5baa60f33e17a7998d2c
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-11-22 13:36:21 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,18 @@
 import "../styles/Footer.css";
+import { Link } from "react-router-dom";
+ 
 const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-container">
                 <div className="footer-section">
-                    <h3>NewsApp</h3>
-                    <p>Your daily source for the latest headlines.</p>
+                    <h3>NewsHorizon</h3>
+                    <p>Beyond The Breaking News.</p>
                 </div>
                 <div className="footer-section">
                     <h4>Quick Links</h4>
                     <ul>
-                        <li><a href="#home">Home</a></li>
+                        <li><Link to="/">Home</Link></li>
                         <li><a href="#about">About</a></li>
                         <li><a href="#contact">Contact</a></li>
                     </ul>
@@ -18,11 +20,9 @@ const Footer = () => {
                 <div className="footer-section">
                     <h4>Follow Us</h4>
                     <div className="social-icons">
-                        
-                        
-                        
+                    
                     </div>
-                </div>
+            </div>
             </div>
             <div className="footer-bottom">
                 <p>© 2024 NewsApp. All rights reserved.</p>
