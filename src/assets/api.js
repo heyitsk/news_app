cfe470954b347b9470c28ab33842e77c2cc77496
Kushagra Agarwal <Kushagradpr2005@gmail.com>
2024-11-22 13:35:32 +0530

    all apis stored

--- src/assets/api.js
+++ src/assets/api.js
@@ -1,5 +1,16 @@
-export const API_KEY = "3a715e2372ef43199f90feaa865c8d26";
+export const API_KEY = "&apiKey=3a715e2372ef43199f90feaa865c8d26";
 
 export const API_US_NEWS = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=3a715e2372ef43199f90feaa865c8d26";
 
 export const SPORTS_API = "https://newsapi.org/v2/everything?q=sports&apiKey=3a715e2372ef43199f90feaa865c8d26";
+
+
+export const SEARCH_API = "https://newsapi.org/v2/everything?q=" 
+
+export const HEADLINES_API = "https://newsapi.org/v2/top-headlines?country=us&apiKey=3a715e2372ef43199f90feaa865c8d26";
+
+export const ARTS_API = "https://newsapi.org/v2/everything?q=art&apiKey=3a715e2372ef43199f90feaa865c8d26";
+
+export const TRAVEL_API = "https://newsapi.org/v2/everything?q=travel&apiKey=3a715e2372ef43199f90feaa865c8d26";
+
+export const BUSINESS_API = "https://newsapi.org/v2/everything?q=business&apiKey=3a715e2372ef43199f90feaa865c8d26";
